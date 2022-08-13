220ee8548bced7da00fd463d757852d711849af9
mbocianski <miles.a.bocianski@gmail.com>
2022-08-13 16:52:33 -0700

    Updates

---
+++ precache-manifest.9a7e447f7b96cf6d509391edee1ec43e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da9e84ba92ac10d8dcfbe23b99346c53",
+    "url": "/recipe-tracking-app/index.html"
+  },
+  {
+    "revision": "c7edc71560332ea4205a",
+    "url": "/recipe-tracking-app/static/css/main.92f86582.chunk.css"
+  },
+  {
+    "revision": "8fc9c2d12fcbb8dbc981",
+    "url": "/recipe-tracking-app/static/js/2.da172efb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/recipe-tracking-app/static/js/2.da172efb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7edc71560332ea4205a",
+    "url": "/recipe-tracking-app/static/js/main.54cf93d7.chunk.js"
+  },
+  {
+    "revision": "74c8d19a009fbd10282d",
+    "url": "/recipe-tracking-app/static/js/runtime-main.457e4dde.js"
+  }
+]);
